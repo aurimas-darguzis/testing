15775f064eff79441a4b35d974edb638a463fb02
aurimas-darguzis <aurimas@darguzis.com>
2017-07-25 16:14:33 +0100

    added 2 e2e tests

--- Angular/e2e/app.e2e-spec.ts
+++ Angular/e2e/app.e2e-spec.ts
@@ -1,14 +1,34 @@
-import { AngularPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
-describe('angular App', () => {
-  let page: AngularPage;
-
-  beforeEach(() => {
-    page = new AngularPage();
+describe(`Page: Joke Page`, () => {
+  it(`should have a title of 'Chuck Norris Jokes'`, () => {
+    browser.get('/');
+    const title = element(by.css('h1')).getText();
+    expect(title).toEqual('Chuck Norris Jokes');
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+  it(`should have different second joke`, async () => {
+    browser.get('/');
+    const joke1 = element(by.css('p')).getText();
+    element(by.css('button')).click();
+    const joke2 = await element(by.css('p')).getText();
+    expect(joke1).not.toEqual(joke2);
+
+  })
 });
+
+// Default setup:
+// import { AngularPage } from './app.po';
+
+// describe('angular App', () => {
+//   let page: AngularPage;
+
+//   beforeEach(() => {
+//     page = new AngularPage();
+//   });
+
+//   it('should display message saying app works', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('app works!');
+//   });
+// });
